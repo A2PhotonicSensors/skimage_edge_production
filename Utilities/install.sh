3726b284faf5ca26e1636a7492304c069280c8a2
A2PhotonicSensors <alefebvre@a2photonicsensors.com>
2020-12-14 15:38:57 +0100

    Added 3 IDs for Les Arcs + updated install.sh

--- Utilities/install.sh
+++ Utilities/install.sh
@@ -8,32 +8,27 @@ echo "Starting installation script on remote odroid. . ."
 echo "Sudo-ing"
 sudo false
 
-echo "Stopping docker" # Avoid E: Unable to acquire the dpkg frontend lock
-docker stop $(docker ps -a -q)
-
-# Load skimage variables
 echo " Loading skimage variables . . . "
-source "skimage_variables.env"
+source $(dirname $BASH_SOURCE)/skimage_variables.env
 
-echo "Removing ${ROOT_DIR}/${SOURCE_DIR}"
-cd 
-sudo rm -rf "${ROOT_DIR}/${SOURCE_DIR}"
-echo "Removed ${ROOT_DIR}/${SOURCE_DIR}"
+echo "Removing $ROOT_DIR/$SOURCE_DIR"
+sudo rm -rf "$ROOT_DIR/$SOURCE_DIR"
 
-# Set time zone
-echo "Setting time zone"
-sudo timedatectl set-timezone ${TZ}
+#echo "Setting time zone"
+#sudo timedatectl set-timezone $TZ
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common \
+	inotify-tools \
+	git
 
-# Install docker
 echo "Installing docker"
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 
@@ -53,65 +48,46 @@ else
 fi
 
 sudo apt-get -y update
-
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
 sudo groupadd docker
-
 sudo usermod -aG docker $USER
 
-echo "Docker installed"
-
-# Install docker-compose
 echo "Installing docker-compose"
-sudo apt-get -y install docker-compose
-echo "docker-compose installed"
+#sudo apt-get -y install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.17.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-# Remove all docker images
-echo "Remove all docker images"
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images)
-echo "Docker images have been removed"
+echo "Pulling docker image"
+sudo docker pull $DOCKER_IMAGE
 
-# Pull Docker image
-echo "Pull docker image"
-docker pull ${DOCKER_IMAGE}
-echo "Docker image pulled"
-
-echo "Installing git"
-sudo apt-get -y install git
+echo "Making odroid directory "
+sudo mkdir -p $ROOT_DIR/$SOURCE_DIR
+sudo chown -hR $USER:users $ROOT_DIR
 
 # clone Github repo
-echo "Cloning into github repo ${GIT_REPO}"
-git clone ${GIT_REPO}
+echo "Cloning into github repo $GIT_REPO"
+git clone $GIT_REPO $ROOT_DIR/$SOURCE_DIR
 echo "Github repo has been pulled"
 
 echo "Making data directory "
-mkdir -p "${ROOT_DIR}/${SOURCE_DIR}/data"
+mkdir -p $ROOT_DIR/$SOURCE_DIR/data
 echo "Copying default skimage_parameters.xlsx' from /docs to /data"
-sudo cp "${ROOT_DIR}/${SOURCE_DIR}/docs/skimage_parameters.xlsx" "${ROOT_DIR}/${SOURCE_DIR}/data/skimage_parameters.xlsx"
+cp $ROOT_DIR/$SOURCE_DIR/docs/skimage_parameters.xlsx $ROOT_DIR/$SOURCE_DIR/data/skimage_parameters.xlsx
 echo "Copying default my_id.txt' from /docs to /data"
-sudo cp "${ROOT_DIR}/${SOURCE_DIR}/docs/my_id.txt" "${ROOT_DIR}/${SOURCE_DIR}/data/my_id.txt"
+cp $ROOT_DIR/$SOURCE_DIR/docs/my_id.txt $ROOT_DIR/$SOURCE_DIR/data/my_id.txt
 
-# Allow skimage.sh to be executable 
-echo "Setting execute permissions on skimage.sh"
-chmod +x "${ROOT_DIR}/${SOURCE_DIR}/skimage.sh"
-
-# Install inotify-tools (Necessary for monitoring of semaphore file by watchdog)
-echo "Installing inotify-tools"
-sudo apt-get -y install inotify-tools
-echo "Inotify-tools installed"
-
-# Set up link to skimage logs folder
 echo "Making Logs_SKIMAGE directory if it doesn't already exist"
-mkdir -p "${ROOT_DIR}/${SOURCE_DIR}/${SKIMAGE_LOGS_DIR}" 
+mkdir -p $ROOT_DIR/$SOURCE_DIR/$SKIMAGE_LOGS_DIR
 
-# Copy skimage_watchdog.service to /lib/systemd/system
+echo "Setting execute permissions on skimage.sh"
+chmod +x $ROOT_DIR/$SOURCE_DIR/skimage.sh
+
 echo "Copying skimage_watchdog.service to /lib/systemd/system"
-sudo cp "${ROOT_DIR}/${SOURCE_DIR}/Utilities/skimage_watchdog.service" /lib/systemd/system
+sudo cp $ROOT_DIR/$SOURCE_DIR/Utilities/skimage_watchdog.service /lib/systemd/system
 
-# Enable service
 echo "Reloading systemd daemon and enabling skimage_watchdog service"
 sudo systemctl daemon-reload
 sudo systemctl enable skimage_watchdog.service
+
+echo "Cleaning apt"
+sudo apt -y autoremove
